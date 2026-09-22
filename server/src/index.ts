eed9026e9896b992779c36f9551e79a0b015826a
Oramulu Emmanuel <oramuluemmanuel294@gmail.com>
2026-09-22 23:45:36 +0100

    Allow cross-origin resource policy for static uploads

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,9 +27,11 @@ emitter.on('user:registered',async ({
 });
 
 const app=express();
-app.set('trust proxy', 1);
+app.set('trust proxy',1);
 app.use(cors(CORS_CONFIG_OPTIONS));
-app.use(helmet());
+app.use(helmet({
+	crossOriginResourcePolicy: { policy: 'cross-origin' },
+}));
 app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser())
 
